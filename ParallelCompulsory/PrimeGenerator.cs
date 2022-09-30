1a39ff56ebb84353257fa3d536d65ca8b9221c73
daiperboevs42 <54886017+daiperboevs42@users.noreply.github.com>
2022-09-30 12:43:40 +0200

    initial commit

---
+++ ParallelCompulsory/PrimeGenerator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelCompulsory
+{
+    public class PrimeGenerator
+    {
+        public List<long> GetPrimesSequential(long first, long last)
+        {
+            List<long> primes = new List<long>();
+            for (var number = first; number < last; number++)
+            {
+                if (IsPrime(number))
+                {
+                    primes.Add(number);
+                }
+            }
+            return primes;
+        }
+
+        static bool IsPrime(long number)
+        {
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+            for (long divisor = 3; divisor < (number / 2); divisor += 2)
+            {
+                if (number % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        public List<long> GetPrimesParallel(long first, long last)
+        {
+            List<long> primes = new List<long>();
+            var lockObject = new object();
+
+            var range = last - first;
+            var numberOfThreads = (long)Environment.ProcessorCount;
+
+            var threads = new Thread[numberOfThreads];
+            var chunkSize = range / numberOfThreads;
+
+            for (long i = 0; i < numberOfThreads; i++)
+            {
+                var chunkStart = first + i * chunkSize;
+                var chunkEnd = (i == (numberOfThreads - 1)) ? last : chunkStart + chunkSize;
+                threads[i] = new Thread(() =>
+                {
+                    for (var number = chunkStart; number < chunkEnd; ++number)
+                    {
+                        if (IsPrime(number))
+                        {
+                            lock (lockObject)
+                            {
+                                primes.Add(number);
+                            }
+                        }
+                    }
+                });
+
+                threads[i].Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+            return primes;
+        }
+
+        static List<long> GeneratePrimes(long start, long end)
+        {
+            bool isPrime = true;
+            List<long> primes = new List<long>();
+            for (long i = start; i <= end; i++)
+            {
+                for (long j = start; j <= end; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primes.Add(i);
+                }
+                isPrime = true;
+            }
+            return primes;
+        }
+
+    }
+}
---
+++ TestOutput/Program.cs
@@ -0,0 +1,42 @@
+﻿using ParallelCompulsory;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        long start = 1000000;
+        long end = 2000000;
+        List<long> primes = new List<long>();
+        var primeGen = new PrimeGenerator();
+        Console.WriteLine("Press any key to start sequential");
+        Console.ReadKey();
+        Console.WriteLine($"Calculating Prime Sequentially from {start} to {end}");
+        MeasureTime(() => primes = primeGen.GetPrimesSequential(start, end));
+        //PrintPrimes(primes);
+
+        Console.WriteLine("Press any key to start parallel");
+        Console.ReadKey();
+        MeasureTime(() => primes = primeGen.GetPrimesParallel(start, end));
+        //PrintPrimes(primes);
+    }
+
+    static void PrintPrimes(List<long> calculatedPrimes)
+    {
+        Console.WriteLine("Press any key to see primes");
+        Console.ReadKey();
+        foreach (var item in calculatedPrimes)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    static void MeasureTime(Action ac)
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        ac.Invoke();
+        sw.Stop();
+        Console.WriteLine("Time = {0} seconds", sw.Elapsed.TotalSeconds);
+    }
+
+}
